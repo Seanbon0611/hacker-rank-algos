fdaa0f4ed808cda1537a6db64b2b97c8b5159a79
Seanbon0611 <seand0611@gmail.com>
2020-09-29 17:52:52 -0700

    added arrays to test as well as a note

--- compare-the-triplets/compareTriplets.rb
+++ compare-the-triplets/compareTriplets.rb
@@ -1,7 +1,11 @@
+a = [4,22,6]
+b = [1,22,20]
+
 def compareTriplets(a, b)
     alices_score = 0
     bobs_score = 0
    a.each_with_index do |v, i|
+    #if the value is greater than the value stored at b array's index
     if v > b[i]
         alices_score += 1
     elsif v < b[i]
