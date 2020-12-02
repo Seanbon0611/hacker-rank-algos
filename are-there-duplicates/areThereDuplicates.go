4d391e29619910bc9ba7c01b8d421c3a60e83f6d
Seanbon0611 <seand0611@gmail.com>
2020-12-01 23:52:44 -0800

    added notes

--- are-there-duplicates/areThereDuplicates.go
+++ are-there-duplicates/areThereDuplicates.go
@@ -7,17 +7,20 @@ import (
 
 //pointers solution
 func areThereDuplicates(a ...int) bool {
-	sort.Ints(a)
-	start := 0
-	next := 1
+	sort.Ints(a) //sort the slice
+	start := 0   // start at 0 index
+	next := 1    //the next element after the start index
 	for next < len(a) {
+		//because the array is sorted, if the index of start and index of next are the same, then we've found a duplicate
 		if a[start] == a[next] {
 			return true
 		} else {
+			//if the two elements are not the same then move onto the next set of elements
 			start++
 			next++
 		}
 	}
+	//if there are no duplicates then we return false
 	return false
 }
 
