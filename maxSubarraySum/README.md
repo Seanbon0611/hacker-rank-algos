5a436de578d3d2c5c03273aac42c4c4df451abef
Seanbon0611 <seand0611@gmail.com>
2020-12-01 16:32:12 -0800

    added maxSubarraySum algo, this is an example of sliding window patter

---
+++ maxSubarraySum/README.md
@@ -0,0 +1,9 @@
+### MAX SUBARRAY SUM
+
+<p> Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array.
+
+```go
+//Example 
+
+maxSubarraySum([1,2,5,2,8,1,5], 2) //10
+```
---
+++ maxSubarraySum/maxSubarraySum.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Sliding window method
+func maxSubarraySum(array []int, n int) (int, error) {
+	if n > len(array) {
+		return 0, nil
+	}
+	maxSum := 0
+	tempSum := 0
+	//Loop through 1 time to get the sum of the first set of numbers
+	for i := 0; i < n; i++ {
+		maxSum += array[i]
+	}
+	//now that we have a max sum, assign temp sum to max sum
+	tempSum = maxSum
+	//loop through the length of the whole array starting from the index of the next number after the first set
+	for i := n; i < len(array); i++ {
+		//assigning new tempsum, we get the current tempSum, move down the number of indexes == to n, subtract that number, then add the number of the current loops index
+		tempSum = tempSum - array[i-n] + array[i]
+		//see which is larger the current maxSum or the tempSum we're holding, if tempSum is larger maxSum is now the value of TempSum
+		maxSum = Max(maxSum, tempSum)
+	}
+	return maxSum, nil
+}
+
+//helper function to get the higher of two integers
+func Max(x int, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+func main() {
+	fmt.Println(maxSubarraySum([]int{1, 1, 2, 4, 2, 3, 5, 1}, 4))
+	fmt.Println(maxSubarraySum([]int{}, 4))
+}
